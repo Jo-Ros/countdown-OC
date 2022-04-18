8f2194fdee6e1798a6ef0767787f4b03ed4e59f5
Jo-Ros <94191307+Jo-Ros@users.noreply.github.com>
2022-04-18 20:03:51 +0200

    countDown app

---
+++ app.js
@@ -0,0 +1,24 @@
+const countdown = () => {
+    const countDate = new Date("August 3, 2022 00:00:00").getTime();
+    const currentDate = new Date().getTime();
+    
+    const gap = countDate - currentDate;
+    
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const dayDisplay = Math.floor(gap / day);
+    const hourDisplay = Math.floor((gap % day) / hour); // check later
+    const minuteDisplay = Math.floor((gap % hour) / minute);
+    const secondDisplay = Math.floor((gap % minute) / second);
+    console.log(secondDisplay);
+
+    document.querySelector('.day').innerText = dayDisplay;
+    document.querySelector('.hour').innerText = hourDisplay;
+    document.querySelector('.minute').innerText = minuteDisplay;
+    document.querySelector('.second').innerText = secondDisplay;
+}
+
+setInterval(countdown, 1000);
